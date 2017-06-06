b3bb7e429f42a2cb71e06226b72a1bca0ee26a6f
Hagai Zamir <hagai34@gmail.com>
2017-06-06 21:32:29 +0300

    RecyclerView to Ynet

--- app/src/main/java/hagai/edu/xmlandfirebase/MainActivity.java
+++ app/src/main/java/hagai/edu/xmlandfirebase/MainActivity.java
@@ -1,6 +1,8 @@
 package hagai.edu.xmlandfirebase;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -13,6 +15,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -21,6 +25,17 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        FirebaseAuth.getInstance().addAuthStateListener(new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser()== null){
+                    Intent intent = new Intent(MainActivity.this , LoginActivity.class);
+                    startActivity(intent);
+
+                }
+            }
+        });
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
